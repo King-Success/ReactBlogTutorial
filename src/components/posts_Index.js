0784720d986712edae4b55f38c24a38dc7102913
King-Success <okonkwoak@gmail.com>
2018-05-15 15:54:21 +0100

    Created the landing page/component and assigned a route to it

---
+++ src/components/posts_Index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div> List of books to be displayed </div>
+    );
+};
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,11 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app.js';
+import postIndex from './components/posts_index.js';
 
-const greetings = () => {
-    return (
-        <div> 
-            Hey there!
-        </div>
-    );
-};
 
 export default (
     <Route path="/" component={App}>
-        <Route path="greetings" component={greetings} />
+        <IndexRoute component={postIndex} />
     </Route>
 );
