4a53453a3e77e5df37a01ab1c486cdc9a172a4d1
King-Success <okonkwoak@gmail.com>
2018-05-15 16:36:32 +0100

    created the action creator responsible for making api request and configured it to pass through the promise middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
-export function selectBook(book) {
+import axios from 'axios';
+
+export const FETCH_POST = 'FETCH_POSTS';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
+const API_KEY = '?key=asdfljh';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}posts${API_KEY}`);
+
     return {
-        type: 'SELECT_BOOK',
-        payload: book
+        type: FETCH_POST,
+        payload: request
     };
 }
