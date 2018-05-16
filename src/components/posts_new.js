5b7fe2c8bd8cd12153f1584033edf14b5beab665
King-Success <okonkwoak@gmail.com>
2018-05-16 14:15:32 +0100

    Handed over control of form elements to redux form and passed the the handleSubmit helper function to the onSubmit event handler

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,25 +4,27 @@ import { reduxForm } from 'redux-form';
 class postsNew extends Component {
 
     render() {
+
+        const { fields: { title, categories, content}, handleSubmit } = this.props;
         return (
             <form>
                 <h3>Create A New Post</h3>
                 <div className="form-group">
                     <label>Title</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" {...title} />
                 </div>
 
                 <div className="form-group">
                     <label>Categories</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control"  {...categories} />
                 </div>
 
                 <div className="form-group">
                     <label>Content</label>
-                    <textarea  className="form-control" />
+                    <textarea  className="form-control" {...content} />
                 </div>
 
-                <button type="submit" className="btn btn-primary" />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
