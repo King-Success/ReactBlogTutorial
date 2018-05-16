a513511822d129a1d7c8036fc227f11101401a85
King-Success <code4kings@gmail.com>
2018-05-16 21:12:34 +0100

    Added a danger color to the error message to make it more noticable

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,7 +10,7 @@ class postsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create A New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
                     <div className="text-help">
@@ -18,7 +18,7 @@ class postsNew extends Component {
                     </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control"  {...categories} />
                     <div className="text-help">
@@ -26,7 +26,7 @@ class postsNew extends Component {
                     </div>
                 </div>
 
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea  className="form-control" {...content} />
                     <div className="text-help">
