537c7bb4e9692101c9082674f25aeb670dad52a5
King-Success <okonkwoak@gmail.com>
2018-05-16 13:53:43 +0100

    connected the redux form to post new component and created an html form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class postsNew extends Component {
 
     render() {
         return (
-            <div> This is the create post Component </div>
+            <form>
+                <h3>Create A New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea  className="form-control" />
+                </div>
+
+                <button type="submit" className="btn btn-primary" />
+            </form>
         );
     }
 }
 
-export default postsNew;
+export default reduxForm({
+    form: 'postsNewForm',
+    fields: ['title', 'categories', 'content']
+})(postsNew);
