64aa32a8584b06f39df0a70b4b069f6f7f725b43
King-Success <code4kings@gmail.com>
2018-05-16 17:05:16 +0100

    Created the createPost action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POST = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=asdfljh';
 
@@ -11,4 +12,13 @@ export function fetchPosts() {
         type: FETCH_POST,
         payload: request
     };
+}
+
+export function createPost(props) {
+    const request = axios.post(`${ROOT_URL}posts${API_KEY}`, props);
+
+    return {
+        type: CREATE_POST, 
+        payload: request
+    }
 }
