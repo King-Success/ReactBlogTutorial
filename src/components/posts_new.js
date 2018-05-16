cb026d42d9ef74635698f4a2f7607aa768af0f33
King-Success <code4kings@gmail.com>
2018-05-16 15:30:42 +0100

    Testing git email setup

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -7,25 +7,25 @@ class postsNew extends Component {
 
         const { fields: { title, categories, content}, handleSubmit } = this.props;
         return (
-            <form>
-                <h3>Create A New Post</h3>
-                <div className="form-group">
-                    <label>Title</label>
-                    <input type="text" className="form-control" {...title} />
-                </div>
+            // <form onSubmit={handleSubmit}>
+            //     <h3>Create A New Post</h3>
+            //     <div className="form-group">
+            //         <label>Title</label>
+            //         <input type="text" className="form-control" {...title} />
+            //     </div>
 
-                <div className="form-group">
-                    <label>Categories</label>
-                    <input type="text" className="form-control"  {...categories} />
-                </div>
+            //     <div className="form-group">
+            //         <label>Categories</label>
+            //         <input type="text" className="form-control"  {...categories} />
+            //     </div>
 
-                <div className="form-group">
-                    <label>Content</label>
-                    <textarea  className="form-control" {...content} />
-                </div>
+            //     <div className="form-group">
+            //         <label>Content</label>
+            //         <textarea  className="form-control" {...content} />
+            //     </div>
 
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            //     <button type="submit" className="btn btn-primary">Submit</button>
+            // </form>
         );
     }
 }
