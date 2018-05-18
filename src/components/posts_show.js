6af7d5b3b97da429c168da8370275e252eef7411
King-Success <code4kings@gmail.com>
2018-05-18 19:49:32 +0100

    Created the html for displaying the content of selected post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -10,11 +10,20 @@ class PostsShow extends Component {
     }
 
     render() {
-        if(!this.props.post) {
+
+        const { post } = this.props;
+
+        if(!post) {
             return <div>Loading...</div>
         }
-        
-        return <div> This is post { this.props.params.id } </div>
+
+        return (
+        <div>
+            <h3>{post.title}</h3>
+            <h6>Categories:{post.categories}</h6>
+            <p>{post.content}</p>
+        </div>
+        );
     }
 }
 
